ef53aa49629237d4e9e0d09d9f24ff2553279259
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-16 22:10:46 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -5,32 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-
-        def isVowels(c):
-            if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
-                return True
-            else:
-                return False
-        
-        count = 0
-        for i in range(k):
-            if isVowels(s[i]) is True:
-                count += 1
-        
-        res = count
-        # print(res)
-        back = 0
-        front = k-1
-        while front < len(s)-1:
-
-            if isVowels(s[back]) is True:
-                count -= 1
-            back += 1
-            front += 1
-            if isVowels(s[front]) is True:
-                count += 1
-             
-            res = max(res, count)
-        
+        res = vowel = 0
+        for i, c in enumerate(s):
+            if c in "aeiou":
+                vowel += 1
+            
+            left = i - k + 1
+            if left < 0:
+                continue
+            
+            res = max(res, vowel)
+            
+            if s[left] in "aeiou":
+                vowel -= 1
         return res
         
