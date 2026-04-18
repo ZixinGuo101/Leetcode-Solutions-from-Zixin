eef7eb1709be17e85e354fecd5051820b67fe2b7
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-19 00:06:54 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.py
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,37 +5,22 @@ class Solution(object):
         :rtype: int
         """
         n = len(height)
-        if n <= 2:
-            return 0
-        left = 1
-        right = n - 2
-        cnt = 0
-        pre = height[0]
-        suf = height[n-1]
+        left = 0
+        right = n - 1
+        pre_max = 0
+        suf_max = 0
+        area = 0
 
-        while left <= right:
-            if pre > suf:
-                if height[right] >= suf:
-                    suf = height[right]
-                else:
-                    cnt += suf - height[right]
-                right -= 1
-            else:
-                if height[left] >= pre:
-                    pre = height[left]
-                else:
-                    cnt += pre - height[left]
+        while left < right:
+            pre_max = pre_max if pre_max > height[left] else height[left]
+            suf_max = suf_max if suf_max > height[right] else height[right]
+            if pre_max < suf_max:
+                area += pre_max - height[left]
                 left += 1
-            print(cnt)
+            else:
+                area += suf_max - height[right]
+                right -= 1
         
-        return cnt
-        '''
-                  -
-        -         -
-        -     -   -
-        - -   - - -
-        - -   - - -
-        - - - - - -
-        '''
-
+        return area
+        
         
