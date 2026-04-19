35cd7756c1e66ea4eb3f55e4adb7e51a3862281e
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-19 17:00:21 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ LeetCode/Medium/2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -5,38 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        len_sub = []
-        min_len = float('inf')
-        cnt_k = 0
-        left_sub = []
-        left = 0
+        if s.count('1') < k:
+            return ''
 
+        cntk = 0
+        ans = ''
+        min_len = int
+        left = 0
         for right, c in enumerate(s):
-            if c == '1':
-                cnt_k += 1
-            
-            while left <= right and cnt_k == k:
-                l = right - left + 1
-                if l <= min_len:
-                    left_sub.append(left)
-                    len_sub.append(l)
-                    min_len = l
-                if s[left] == '1':
-                    cnt_k -= 1
+            cntk += int(c)
+
+            while s[left] == '0' or cntk > k:
+                cntk -= int(s[left])
                 left += 1
 
-        if min_len == float('inf'):
-            return ""
+            if cntk == k:
+                l = right - left + 1
+                res = s[left : right+1]
+                if ans == '' or l < len(ans) or (l == len(ans) and res < ans):
+                    ans = res
         
-        res_left = []
-        for i in range(len(len_sub)):
-            if len_sub[i] == min_len:
-                res_left.append(left_sub[i])
+        return ans
+                
         
-        res = '1'*min_len
-        for i in range(len(res_left)):
-            if s[res_left[i]:(res_left[i]+min_len)] < res:
-                res = s[res_left[i]:(res_left[i]+min_len)]
-        
-        return res
-            
