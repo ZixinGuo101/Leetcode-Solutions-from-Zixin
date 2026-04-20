a29cfb5f3ef4578f51fc2a5bf85750dbccf8d357
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-20 23:37:33 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.py
+++ LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -7,31 +7,33 @@ class Solution(object):
         """
         m = len(s)
         n = len(t)
-        min_len = len(s) + 1
-        left = 0
-        min_left = 0
-        if m < n:
-            return ''
-        
-        d = dict()
-        for i, c in enumerate(t):
-            d[c] = d.get(c, 0) + 1
-        
-        left  = 0
-        for right, c in enumerate(s):
-            if c in d:
-                d[c] -= 1
+        need = dict()
+        window = dict()
 
-            while left <= right and max(d.values()) <= 0:
+        for i in range(n):
+            need[t[i]] = need.get(t[i], 0) + 1
+        
+        valid = 0
+        left = 0
+        min_len = len(s) + 1
+        min_left = 0
+        for right, c in enumerate(s):
+            if c in need:
+                window[c] = window.get(c, 0) + 1
+                if window[c] == need[c]:
+                    valid += 1
+            
+            while valid == len(need):
                 l = right - left + 1
                 if l <= min_len:
                     min_len = l
                     min_left = left
-                
-                if s[left] in d:
-                    d[s[left]] += 1
+                if s[left] in need:
+                    window[s[left]] -= 1
+                    if window[s[left]] < need[s[left]]:
+                        valid -= 1
                 left += 1
-        
+            
         return s[min_left : min_left + min_len] if min_len < len(s) + 1 else ''
 
-            
+        
