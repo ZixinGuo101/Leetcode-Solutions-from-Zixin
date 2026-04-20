08c42f56feb93d5f0f2ed2f09f1680e3032ed18e
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-20 21:10:20 +0800

    Time: 6 ms (55.42%), Space: 12.2 MB (92.77%) - LeetHub

--- LeetCode/Easy/3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ LeetCode/Easy/3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -5,20 +5,18 @@ class Solution(object):
         :rtype: int
         """
         ans = 0
-        cnt = [0] * 26
         left = 0
+        d = dict()
 
         for right, c in enumerate(s):
-            x = ord(c) -97
-            cnt[x] += 1
+            d[c] = d.get(c, 0) + 1
 
-            while cnt[x] > 2:
-                y = ord(s[left]) - 97
-                cnt[y] -= 1
-                left += 1            
-
+            while d[c] > 2:
+                d[s[left]] -= 1
+                left += 1
+            
             l = right - left + 1
-            ans = l if l > ans else ans
+            ans  = l if l > ans else ans
         
         return ans
         
