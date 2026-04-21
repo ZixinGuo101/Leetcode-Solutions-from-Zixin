bbf8ff87f699672899a10e80ada57f5d24347c9e
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-21 11:12:26 +0800

    Time: 62 ms (98.11%), Space: 13 MB (64.36%) - LeetHub

--- LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.py
+++ LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -7,7 +7,6 @@ class Solution(object):
         """
         m = len(s)
         n = len(t)
-
         if m < n:
             return ''
         
@@ -16,23 +15,26 @@ class Solution(object):
             d[t[i]] = d.get(t[i], 0) + 1
         
         left = 0
-        min_len = m + 1
-        min_left = 0
+        m_len = m + 1
+        start = 0
+        k = n
         for right, c in enumerate(s):
             if c in d:
                 if d[c] > 0:
-                    n -= 1
+                    k -= 1
                 d[c] -= 1
             
-            while n == 0:
+            while k == 0:
                 l = right - left + 1
-                if l < min_len:
-                    min_len = l
-                    min_left = left
+                if l < m_len:
+                    m_len = l
+                    start = left
+                
                 if s[left] in d:
                     d[s[left]] += 1
                     if d[s[left]] > 0:
-                        n += 1
+                        k += 1
                 left += 1
         
-        return s[min_left : min_left + min_len] if min_len < (m + 1) else ''
+        return s[start : start + m_len] if m_len < m + 1 else ''
+        
