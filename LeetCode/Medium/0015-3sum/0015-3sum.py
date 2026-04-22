1b7dab510dfc3e5d3ba9d65372c028dedbb31524
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-22 23:23:36 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.py
+++ LeetCode/Medium/0015-3sum/0015-3sum.py
@@ -10,26 +10,30 @@ class Solution(object):
         for i, num in enumerate(nums):
             if i > 0 and num == nums[i-1]:
                 continue
-            print(num)
+            # print(num)
             cnt = -num
             l = i+1
             r = n-1
 
             while l < r:
+                # print(l, r)
                 sn = nums[l] + nums[r]
                 ln, rn = nums[l], nums[r]
                 if  sn < cnt:
-                    l += 1   
+                    while l < r and nums[l] == ln:
+                        l += 1   
                 elif sn > cnt:
-                    r -= 1
+                    while l < r and nums[r] == rn:
+                        r -= 1
                 else:
                     ans.append([num, nums[l], nums[r]])
                     l += 1
                     r -= 1
-                while l < r and nums[l] == ln:
-                    l += 1
-                while l < r and nums[r] == rn:
-                    r -= 1
+                    while l < r and nums[l] == ln:
+                        l += 1
+                    while l < r and nums[r] == rn:
+                        r -= 1
+                
                 
         
         return ans
