73d4947253c1e2f08c109691cf5e94e3921754ca
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-22 16:45:01 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/3794-reverse-string-prefix/3794-reverse-string-prefix.py
+++ LeetCode/Easy/3794-reverse-string-prefix/3794-reverse-string-prefix.py
@@ -5,13 +5,6 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        s = list(s)
-        l = 0
-        r = k-1
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
-        
-        return ''.join(s)
-        
+        s1 = s[k-1::-1]
+        s2 = s[k:]
+        return s1+s2
