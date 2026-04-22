0baed9a5cf0e6bdf134d85e7e1d2f513f4a6d366
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-22 20:09:48 +0800

    Time: 3 ms (19.33%), Space: 12.4 MB (24.67%) - LeetHub

---
+++ LeetCode/Easy/0832-flipping-an-image/0832-flipping-an-image.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for row in image:
+            l = 0
+            r = len(row) - 1
+            while l <= r:
+                if row[l] == row[r]:
+                    row[l] = row[r] = row[l] ^ 1
+                l += 1
+                r -= 1
+        
+        return image
+
+        
