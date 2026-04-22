b192dc04d048f38d1ec1d39138e47e733a549a34
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-22 16:28:57 +0800

    Time: 0 ms (100%), Space: 19.9 MB (48.57%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        p1 = 0
-        p2 = len(s) - 1
+        n = len(s)
+        left = 0
+        right = n - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
         
-        while p1 < p2:
-            s[p1], s[p2] = s[p2], s[p1]
-            p1 += 1
-            p2 -= 1
-        
-        return
-        
+        return s
