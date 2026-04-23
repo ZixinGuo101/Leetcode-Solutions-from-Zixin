1b4f19adee4bc3ec14a6b97597c67a6045d61f4c
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-23 21:00:24 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0611-valid-triangle-number/0611-valid-triangle-number.py
+++ LeetCode/Medium/0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -7,17 +7,17 @@ class Solution(object):
         nums.sort()
         n = len(nums)
         ans = 0
-        m = n - 1
+        m = n-1
 
         while m > 1:
             l = 0
-            r = m - 1
+            r= m-1
             while l < r:
-                while l < r and nums[l] + nums[r] <= nums[m]:
+                if nums[l] + nums[r] > nums[m]:
+                    ans += r-l
+                    r -= 1
+                else:
                     l += 1
-                if l < r:
-                    ans += r - l
-                r -= 1
             m -= 1
         
         return ans
