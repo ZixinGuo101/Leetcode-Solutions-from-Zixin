db53d7eb58452d0b49b2bb1569656426001e28ed
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-24 13:12:50 +0800

    Time: 6 ms (24.1%), Space: 12.5 MB (15.66%) - LeetHub

---
+++ LeetCode/Medium/0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def bagOfTokensScore(self, tokens, power):
+        """
+        :type tokens: List[int]
+        :type power: int
+        :rtype: int
+        """
+        tokens.sort()
+        if len(tokens) == 0 or tokens[0] > power:
+            return 0
+        
+        if len(tokens) == 1:
+            return 1
+        
+        l = 0
+        r = len(tokens) - 1
+
+        ans = 0
+        while l <= r:
+            if power >= tokens[l]:
+                power -= tokens[l]
+                l += 1
+                ans += 1
+            elif r-1 > l:
+                power += tokens[r]
+                ans -= 1
+                r -= 1
+            else:
+                return ans
+        
+        return ans
+                
