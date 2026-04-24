0c2189ed3ae335b69ded84ac244c937b32bdf0ff
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-24 10:16:38 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -4,19 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        d = dict()
-        ans = []
+        ans = set()
+        d = set()
         n = len(s)
 
-        for right in range(9, n):
-            left = right - 10 + 1
-            sub = s[left:right+1]
-            if sub in d and d[sub] == 1:
-                d[sub] += 1
-                ans.append(sub)
+        for r in range(9, n):
+            sub = s[r-9:r+1]
+            if sub in d:
+                ans.add(sub)
             else:
-                d[sub] = d.get(sub, 0) + 1
+                d.add(sub)
         
-        return ans
-
-        
+        return list(ans)
