a08932ce3535599bfe269ac9c02fde5ecb516834
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-25 10:28:12 +0800

    Time: 224 ms (31.2%), Space: 13.3 MB (90.54%) - LeetHub

---
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        def EatingHours(k):
+            hours = 0
+            for pile in piles:
+                hours += pile // k
+                if pile % k > 0:
+                    hours += 1
+            return hours
+        
+        piles.sort()
+        l = 1
+        r = piles[len(piles) - 1]
+
+        while l <= r:
+            mid = l + ( r - l) // 2
+            t = EatingHours(mid)
+            # print(mid, t)
+            if t > h:
+                l = mid + 1
+            elif t < h:
+                r = mid - 1
+            elif t == h:
+                r = mid - 1
+        
+        return l
+
+        
