e51a87e0b981e924a19d0aac19abd3a71b047387
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-26 20:30:09 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.py
+++ LeetCode/Medium/3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.py
@@ -5,30 +5,21 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        
-        def eq(i):
-            d = nums2[0] - nums1[i]
-            l1 = i + 1
-            l2 = 1
-            while l1 < m and l2 < n:
-                while l1 < m and nums1[l1] + d != nums2[l2]:
-                    l1 += 1
-                if l1 >= m:
-                    return False
-                l1 += 1
-                l2 += 1
-            
-            return l2 == n
-
         nums1.sort()
         nums2.sort()
         m = len(nums1)
         n = len(nums2)
         ans = float('inf')
 
-        for i in range(3):
-            if eq(i):
-                d = nums2[0] - nums1[i]
-                ans = d if d < ans else ans
+        for i in range(2, 0, -1):
+            d = nums2[0] - nums1[i]
+            j = 1
+            for num1 in nums1[i+1:]:
+                # print(num1)
+                if num1 + d == nums2[j]:
+                    j += 1
+                    print(j)
+                    if j == n:
+                        return d
         
-        return ans
+        return nums2[0] - nums1[0]
