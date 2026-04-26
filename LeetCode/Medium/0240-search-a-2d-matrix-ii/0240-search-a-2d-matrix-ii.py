d383a50627bd37fcd3e3b722f184012b25e81ccb
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-26 23:50:16 +0800

    Time: 131 ms (56.74%), Space: 18.3 MB (83.96%) - LeetHub

---
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        x = 0
+        y = n - 1
+        while x < m and y >= 0:
+            if matrix[x][y] == target:
+                return True
+            elif matrix[x][y] > target:
+                y -= 1
+            elif matrix[x][y] < target:
+                x += 1
+        
+        return False
