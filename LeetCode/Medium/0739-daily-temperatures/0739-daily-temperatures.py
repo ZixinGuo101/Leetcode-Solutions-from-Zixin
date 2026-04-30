bcf194a82d8cdb611bf42ff0615566a52899b775
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-04-30 22:41:01 +0800

    Time: 244 ms (5.82%), Space: 26.9 MB (63.06%) - LeetHub

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
@@ -7,15 +7,10 @@ class Solution(object):
         n = len(temperatures)
         res = [0] * n
         st = []
-
-        for i in range(n-1, -1, -1):
-            while st and temperatures[i] >= temperatures[st[-1]]:
-                st.pop()
-            if st:
-                res[i] = st[-1] - i
-            else:
-                res[i] = 0
+        for i in range(n):
+            while st and temperatures[i] > temperatures[st[-1]]:
+                prev = st.pop()
+                res[prev] = i - prev
             st.append(i)
         
         return res
-            
