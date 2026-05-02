a305e2476c85056444fee739205594c6bd055116
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-05-02 13:16:47 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ LeetCode/Medium/1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -10,11 +10,11 @@ class Solution(object):
         for c in s:
             if c == '(':
                 right += 2
-                if right % 2 == 1:
+                if right % 2 == 1:  # 前面的左括号缺少一个右括号，但已经无法从字符串中继续匹配
                     right -= 1
                     ans += 1
             elif c == ')':
-                if right == 0:
+                if right == 0:  # 前面没有和右括号匹配的左右括号
                     right += 1
                     ans += 1
                 elif right != 0:
