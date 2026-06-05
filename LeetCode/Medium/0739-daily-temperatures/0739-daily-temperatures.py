a3ad67ae2469ac5e7dde5f56d6978c6589e6b128
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-06 00:02:41 +0800

    Time: 214 ms (13.36%), Space: 26.9 MB (61.42%) - LeetHub

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
@@ -5,12 +5,13 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(temperatures)
-        res = [0] * n
-        st = []
-        for i in range(n):
-            while st and temperatures[i] > temperatures[st[-1]]:
-                prev = st.pop()
-                res[prev] = i - prev
-            st.append(i)
+        res = [-1] * n
+        stack = []
+
+        for i in range(n-1, -1, -1):
+            while stack and temperatures[i] >= stack[-1][0]:
+                stack.pop()
+            res[i] = 0 if not stack else (stack[-1][1] - i)
+            stack.append([temperatures[i], i])
         
         return res
