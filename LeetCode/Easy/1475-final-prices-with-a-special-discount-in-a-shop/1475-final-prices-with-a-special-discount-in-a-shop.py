dd77c050542a313d7aca4ac436d89b412b1ede72
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-07 12:56:02 +0800

    Time: 4 ms (60.1%), Space: 12.3 MB (85.35%) - LeetHub

--- LeetCode/Easy/1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ LeetCode/Easy/1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -5,13 +5,11 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(prices)
-        ans = [0] * n
+        ans = prices[:]
         stk = []
         for i in range(n):
             while stk and prices[i] <= prices[stk[-1]]:
                 ans[stk[-1]] = prices[stk[-1]] - prices[i]
                 stk.pop()
             stk.append(i)
-        for i in stk:
-            ans[i] = prices[i]
         return ans
