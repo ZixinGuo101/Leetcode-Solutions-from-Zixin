f4c2284c1d1f27bc7dfbfb404304553fc2bf2417
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-09 16:34:28 +0800

    Time: 14 ms (71.95%), Space: 13.4 MB (27.6%) - LeetHub

--- LeetCode/Medium/0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ LeetCode/Medium/0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -5,20 +5,17 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        l = n
-        r = -1
-        lstk = []
-        rstk = []
+        start = 0
+        end = -1
+        nmax = nums[0]
+        nmin = nums[n-1]
         for i in range(n):
-            while lstk and nums[lstk[-1]] > nums[i]:
-                l = min(l, lstk.pop())
-                # print("l = ", l)
-            lstk.append(i)
-            while rstk and nums[rstk[-1]] < nums[n-1-i]:
-                r = max(r, rstk.pop())
-                # print("r = ", r)
-            rstk.append(n-1-i)
-        if l == n and r == -1:
-            return 0
-        return r - l + 1
-
+            if nmax > nums[i]:
+                end = i
+            else:
+                nmax = nums[i]
+            if nmin < nums[n-1-i]:
+                start = n-1-i
+            else:
+                nmin = nums[n-1-i]
+        return end - start + 1
