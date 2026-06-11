fe77b8a10b1331d5bed6009c73888323995604bf
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-11 17:54:14 +0800

    Time: 283 ms (5.25%), Space: 18.4 MB (6.3%) - LeetHub

--- LeetCode/Medium/0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ LeetCode/Medium/0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        minq = deque()
+        res = float('-inf')
         n = len(nums)
-        sum_nums = 0
-        max_sum = float('-inf')
-        min_sum = float('inf')
-        max_include_i = 0
-        min_include_i = 0
-        for i in range(n):
-            sum_nums += nums[i]
-            max_include_i = max(max_include_i, 0) + nums[i]
-            max_sum = max(max_sum, max_include_i)
-            min_include_i = min(min_include_i, 0) + nums[i]
-            min_sum = min(min_sum, min_include_i)
-        if max_sum <= 0:
-            return max_sum
-        else:
-            return max(max_sum, sum_nums - min_sum)
+        pre_sum = [0] * (2*n+1)
+        start = 0
+        for end in range(1, 2*n+1):
+            pre_sum[end] = pre_sum[end-1] + nums[(end-1) % n]
+            if minq and end - minq[0] > n:
+                minq.popleft()
+            if minq:
+                res = max(res, pre_sum[end] - pre_sum[minq[0]])
+            while minq and pre_sum[minq[-1]] >= pre_sum[end]:
+                minq.pop()
+            minq.append(end)
+        return res
