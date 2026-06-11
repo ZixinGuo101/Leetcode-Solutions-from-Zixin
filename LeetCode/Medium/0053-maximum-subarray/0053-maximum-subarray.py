a21e7c7478189793a484fe9e405cd9bf3aed6819
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-11 12:46:07 +0800

    Time: 83 ms (60.51%), Space: 22 MB (12.87%) - LeetHub

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,13 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
         res = float('-inf')
-        min_sum = 0
-        pre_sum = [0] * (n+1)
-        for i in range(1, n+1):
-            pre_sum[i] = pre_sum[i-1] + nums[i-1]
-            res = max(res, pre_sum[i] - min_sum)
-            if pre_sum[i] < min_sum:
-                min_sum = pre_sum[i]
+        sums = 0
+        for num in nums:
+            sums = max(sums, 0) + num
+            res = max(sums, res)
         return res
