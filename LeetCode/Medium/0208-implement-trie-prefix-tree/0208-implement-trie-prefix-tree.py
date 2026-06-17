5c64c20e5b0b67a7c6adb5a0bb1b59779b01df49
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-17 23:22:39 +0800

    Time: 29 ms (95.86%), Space: 27.7 MB (95.31%) - LeetHub

--- LeetCode/Medium/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ LeetCode/Medium/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,70 +1,45 @@
-class TrieNode:
-    def __init__(self, val = False):
-        self.R = 26
-        self.val = val
-        self.children = [None] * self.R
-
 class Trie(object):
 
     def __init__(self):
-        self.root = TrieNode()
+        self.trie = {}
 
     def insert(self, word):
         """
         :type word: str
         :rtype: None
         """
-        self._insert(self.root, word, 0)
-        return
-
-    def _insert(self, node, word, i):
-        if node is None:
-            node = TrieNode()
-        if i == len(word):
-            node.val = True
-            return node
-        j = ord(word[i]) - ord('a')
-        node.children[j] = self._insert(node.children[j], word, i+1)
-        return node
+        node = self.trie
+        for ch in word:
+            if ch not in node:
+                node[ch] = {}
+            node = node[ch]
+        node["is_word"] = True
+        
 
     def search(self, word):
         """
         :type word: str
         :rtype: bool
         """
-        node = self._search(self.root, word, 0)
-        return node.val
-    
-    def _search(self, node, word, i):
-        if node is None:
-            return self.root
-        if i == len(word):
-            return node
-        j = ord(word[i]) - ord('a')
-        return self._search(node.children[j], word, i+1)
+        node = self.trie
+        for ch in word:
+            if ch not in node:
+                return False
+            node = node[ch]
+        return node.get("is_word", False)
 
     def startsWith(self, prefix):
         """
         :type prefix: str
         :rtype: bool
         """
-        node = self._search(self.root, prefix, 0)
-        if node == self.root:
-            return False
-        else:
-            return True
-    
-    '''
-    def _get(self, node):
-        if node is None:
-            return
-        if node.val is True:
-            return True
-        for i in range(len(node.children)):
-            if node.children[i] is not None:
-                self._get(node.children[i])
-        return False
-    '''
+        node = self.trie
+        for ch in prefix:
+            if ch not in node:
+                return False
+            node = node[ch]
+        return True
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
