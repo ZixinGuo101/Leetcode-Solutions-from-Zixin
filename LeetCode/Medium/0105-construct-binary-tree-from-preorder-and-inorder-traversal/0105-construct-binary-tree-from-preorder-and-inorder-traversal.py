eb41c824b2e082f5fe91f6637130d5d8684f339f
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-20 00:06:37 +0800

    Time: 9 ms (57.49%), Space: 16.3 MB (98.16%) - LeetHub

--- LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -12,6 +12,7 @@ class Solution(object):
         :rtype: Optional[TreeNode]
         """
         n = len(preorder)
+        self.val_to_idx = {val: idx for idx, val in enumerate(inorder)}
         return self.build(preorder, 0, n-1, inorder, 0, n-1)
     
     def build(self, preorder, pl, pr, inorder, il, ir):
@@ -20,15 +21,10 @@ class Solution(object):
         cur = TreeNode(preorder[pl])
         if pl == pr:
             return cur
-        idx = self.findIndex(preorder[pl], inorder, il, ir)
+        idx = self.val_to_idx[preorder[pl]]
         # print(idx)
         # print("preorder_left:", pl+1, pl+idx-il)
         # print("preorder_right", pl+idx-il+1, pr)
-        cur.left = self.build(preorder, pl+1, pl+idx-il, inorder, il, il+idx-1)
+        cur.left = self.build(preorder, pl+1, pl+idx-il, inorder, il, idx-1)
         cur.right = self.build(preorder, pl+idx-il+1, pr, inorder, idx+1, ir)
         return cur
-    
-    def findIndex(self, num, inorder, il, ir):
-        for i in range(il, ir+1):
-            if num == inorder[i]:
-                return i
