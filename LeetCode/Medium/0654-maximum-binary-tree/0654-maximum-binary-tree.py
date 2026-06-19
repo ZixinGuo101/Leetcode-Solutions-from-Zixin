caddbfe031edb4e646f6c00459b033d704b52ef2
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-19 23:05:57 +0800

    Time: 70 ms (15.53%), Space: 13 MB (65.84%) - LeetHub

--- LeetCode/Medium/0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ LeetCode/Medium/0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -10,16 +10,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: Optional[TreeNode]
         """
-        root = None
-        stk = []
-        for num in nums:
-            cur = TreeNode(num)
-            while stk and stk[-1].val < cur.val:
-                cur.left = stk[-1]
-                stk.pop()
-            if not stk:
-                root = cur
-            else:
-                stk[-1].right = cur
-            stk.append(cur)
+        n = len(nums)
+        root = self.construct(nums, 0, n-1)
         return root
+    
+    def construct(self, nums, l, r):
+        if l > r:
+            return None
+        idx = self.max_of_nums(nums, l, r)
+        cur = TreeNode(nums[idx])
+        cur.left = self.construct(nums, l, idx-1)
+        cur.right = self.construct(nums, idx+1, r)
+        return cur
+    
+    def max_of_nums(self, nums, l, r):
+        if l == r:
+            return l
+        idx = l
+        for i in range(l+1, r+1):
+            if nums[i] > nums[idx]:
+                idx = i
+        return idx
+    
