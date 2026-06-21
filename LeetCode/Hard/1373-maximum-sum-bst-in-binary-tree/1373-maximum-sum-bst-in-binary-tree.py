a3ef44c3f6a7582003fcc1f32a04090767a52625
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-21 22:01:33 +0800

    Time: 166 ms (100%), Space: 83.1 MB (74.12%) - LeetHub

--- LeetCode/Hard/1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ LeetCode/Hard/1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -10,33 +10,26 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        self.maxSum = float('-inf')
-        def boundary(root):
-            if root.left is None and root.right is None:
-                self.maxSum = max(self.maxSum, root.val)
-                return root.val, root.val, root.val, True
-            isValid = True
-            lo = root.val
-            hi = root.val
-            left_sum = 0
-            right_sum = 0
-            if root.left:
-                left_lo, left_hi, left_sum, left_isValid = boundary(root.left)
-                if left_hi >= root.val or not left_isValid:
-                    isValid = False
-                else:
-                    lo = left_lo
-            if root.right:
-                right_lo, right_hi, right_sum, right_isValid = boundary(root.right)
-                if right_lo <= root.val or not right_isValid:
-                    isValid = False
-                else:
-                    hi = right_hi
-            if not isValid:
-                return root.val, root.val, root.val, False
-            tree_sum = left_sum + right_sum + root.val
-            self.maxSum = max(self.maxSum, tree_sum)
-            return lo, hi, tree_sum, True
-        boundary(root)
-        return 0 if self.maxSum < 0 else self.maxSum
+        self.res = 0
+
+        def dfs(root):
+            if root is None:
+                return None, None, 0, True
+            l_min, l_max, l_sum, l_is_valid = dfs(root.left)
+            r_min, r_max, r_sum, r_is_valid = dfs(root.right)
+
+            if l_is_valid and r_is_valid and \
+               (l_max is None or root.val > l_max) and \
+               (r_min is None or root.val < r_min):
+               tree_sum = root.val + l_sum + r_sum
+               new_min = l_min if l_min else root.val
+               new_max = r_max if r_max else root.val
+               self.res = max(self.res, tree_sum)
+               return new_min, new_max, tree_sum, True
+            
+            return None, None, 0, False
         
+        dfs(root)
+        return self.res
+
+               
