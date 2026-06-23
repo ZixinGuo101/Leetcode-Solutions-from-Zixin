2e5cbd10c35adbb9bb3dbd424a6d50aa89c69f8a
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-23 17:58:27 +0800

    Time: 1 ms (93.45%), Space: 19.4 MB (71.73%) - LeetHub

--- LeetCode/Easy/1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ LeetCode/Easy/1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,4 +1,11 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        arr.sort(key = lambda x: (x.bit_count(), x))
+        maxNum = 10 ** 4
+        l = maxNum.bit_length()
+        offset = 1 << (l-1).bit_length()
+        for i in range(len(arr)):
+            arr[i] |= arr[i].bit_count() << offset
+        arr.sort()
+        for i in range(len(arr)):
+            arr[i] &= (1 << offset) - 1
         return arr
