eefb9091126991c7e0275fa9362a1d062644f769
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-23 23:42:48 +0800

    Time: 0 ms (100%), Space: 19.1 MB (89.66%) - LeetHub

--- LeetCode/Easy/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
+++ LeetCode/Easy/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -1,12 +1,4 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        l = (n & -n).bit_length()
-        if l > 2:
-            return False
-        n >>= l
-        while n:
-            gap = (n & -n).bit_length()
-            if gap != 2:
-                return False
-            n >>= gap
-        return True
+        x = (n >> 1) ^ n
+        return (x + 1) & x == 0
