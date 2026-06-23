13323d3cb2b0cbcb479d977271110c6ed7c00db9
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-23 20:20:25 +0800

    Time: 1 ms (18.95%), Space: 19.4 MB (12.24%) - LeetHub

--- LeetCode/Easy/0868-binary-gap/0868-binary-gap.py
+++ LeetCode/Easy/0868-binary-gap/0868-binary-gap.py
@@ -1,13 +1,9 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
         res = 0
-        start = (n & -n).bit_length() - 1
-        end = start
-        n = n >> (start + 1)
+        n = n >> (n & -n).bit_length()
         while n:
-            end += 1
-            if n & 1:
-                res = max(res, end - start)
-                start = end
-            n = n >> 1
+            gap = (n & -n).bit_length()
+            res = max(res, gap)
+            n = n >> gap
         return res
