4893ef51651743ce37cda940d2ac07f760567f7c
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-24 17:03:47 +0800

    Time: 0 ms (100%), Space: 19.3 MB (30.31%) - LeetHub

--- LeetCode/Easy/2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ LeetCode/Easy/2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -1,11 +1,9 @@
 class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
-        s = p = 0
+        mask = 0
         for num in nums:
             d, r = divmod(num, original)
             if r == 0 and d & (d - 1) == 0:
-                s |= d
-        while s & 1:
-            p += 1
-            s >>= 1
-        return original * (2 ** p)
+                mask |= d
+        mask = ~mask
+        return original * (mask & -mask)
