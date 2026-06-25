0f32e29be918c706a169151cfa992250a05606a0
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-25 23:32:39 +0800

    Time: 37 ms (54.35%), Space: 29.8 MB (17.39%) - LeetHub

--- LeetCode/Medium/2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
+++ LeetCode/Medium/2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxSubarrays(self, nums: List[int]) -> int:
-        maxNum = (1 << (10 ** 6).bit_length()) - 1
-        acc = m = maxNum
+        acc = -1
         cnt = 0
         for i in range(len(nums)):
-            m &= nums[i]
             acc &= nums[i]
             if acc == 0:
                 cnt += 1
-                acc = maxNum
-        return 1 if m else cnt
+                acc = -1
+        return max(cnt, 1)
