c752b30af076d6586a85576a6603a6d7c790114e
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-26 17:05:56 +0800

    Time: 147 ms (98.33%), Space: 32.3 MB (100%) - LeetHub

--- LeetCode/Hard/3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.py
+++ LeetCode/Hard/3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.py
@@ -1,11 +1,20 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
+        # 滑动窗口 + 双栈
         res = float(inf)
-        for r, num in enumerate(nums):
-            res = min(res, abs(k - num))
-            for l in range(r-1, -1, -1):
-                if nums[l] | num == nums[l] or nums[l+1] > k: # logtrick
-                    break
-                nums[l] |= num
-                res = min(res, abs(k - nums[l]))
+        right_OR = left = bottom = 0
+        for right, num in enumerate(nums):
+            right_OR |= num
+            while left <= right and right_OR | nums[left] > k:
+                res = min(res, (right_OR | nums[left]) - k)
+                left += 1
+                if left > bottom:
+                    for i in range(right - 1, left - 1, -1):
+                        nums[i] |= nums[i+1]
+                    bottom = right
+                    right_OR = 0
+            if left <= right:
+                res = min(res, k - (right_OR | nums[left]))
+            if res == 0:
+                break
         return res
