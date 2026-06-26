77d752835fe48dc664379ca583190d8aa815de97
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-26 22:27:07 +0800

    Time: 0 ms (100%), Space: 19.5 MB (22.37%) - LeetHub

--- LeetCode/Easy/0257-binary-tree-paths/0257-binary-tree-paths.py
+++ LeetCode/Easy/0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -1,32 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def binaryTreePaths(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[str]
-        """
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         self.res = []
         self.path = []
-        self.dfs(root)
-        return self.res
-    
-    def dfs(self, root):
-        if root is None:
-            return
-        if root.left is None and root.right is None:
-            self.path.append(str(root.val))
-            cur = '->'.join(self.path)
-            self.res.append(cur)
+        def traverse(node: Optional[TreeNode]) -> None:
+            if node is None:
+                return
+            self.path.append(str(node.val))
+            if node.left is None and node.right is None:
+                self.res.append('->'.join(self.path))
+            else:
+                traverse(node.left)
+                traverse(node.right)
             self.path.pop()
             return
-        self.path.append(str(root.val))
-        self.dfs(root.left)
-        self.dfs(root.right)
-        self.path.pop()
-        return
-        
+        traverse(root)
+        return self.res
