e100c4306d979db6f308de33b6e72c1e5e87c754
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-26 22:32:01 +0800

    Time: 0 ms (100%), Space: 19.2 MB (70.9%) - LeetHub

--- LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -1,30 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def sumNumbers(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        self.path = []
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        self.dfs(root)
+        def dfs(root, value):
+            if root is None:
+                return
+            new_value = root.val + value * 10
+            if root.left is None and root.right is None:
+                self.res += new_value
+            else:
+                dfs(root.left, new_value)
+                dfs(root.right, new_value)
+            return
+        dfs(root, 0)
         return self.res
-    
-    def dfs(self, root):
-        if root is None:
-            return
-        if root.left is None and root.right is None:
-            self.path.append(str(root.val))
-            self.res += int(''.join(self.path))
-            self.path.pop()
-            return
-        self.path.append(str(root.val))
-        self.dfs(root.left)
-        self.dfs(root.right)
-        self.path.pop()
-        return
