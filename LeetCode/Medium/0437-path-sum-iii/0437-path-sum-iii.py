a318e30f2c34991b3f7bfc26540e12b9eaa4eb04
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-28 23:31:46 +0800

    Time: 3 ms (89.46%), Space: 21 MB (11%) - LeetHub

--- LeetCode/Medium/0437-path-sum-iii/0437-path-sum-iii.py
+++ LeetCode/Medium/0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,22 +6,17 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        d = {}
+        d = defaultdict(int)
         d[0] = 1
         def findSum(root: Optional[TreeNode], lastSum: int) -> int:
             if root is None:
                 return 0
-            cur = root.val + lastSum
-            res = 0
-            if cur - targetSum in d:
-                res = d[cur - targetSum]
-            d[cur] = d.get(cur, 0) + 1
-            l = findSum(root.left, cur)
-            r = findSum(root.right, cur)
-            if d[cur] - 1 == 0:
-                d.pop(cur)
-            else:
-                d[cur] -= 1
+            lastSum += root.val
+            res = d[lastSum - targetSum]
+            d[lastSum] += 1
+            l = findSum(root.left, lastSum)
+            r = findSum(root.right, lastSum)
+            d[lastSum] -= 1
             # print("node: ", root.val, res, l, r)
             return res + l + r
         return findSum(root, 0)
