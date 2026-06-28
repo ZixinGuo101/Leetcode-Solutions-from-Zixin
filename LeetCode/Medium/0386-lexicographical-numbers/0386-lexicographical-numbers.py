d32c3ae55d515c5589755a733ab94bc9d310faa4
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-29 01:05:09 +0800

    Time: 63 ms (14.36%), Space: 22.7 MB (96.07%) - LeetHub

--- LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,16 +1,16 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
+        # 1, 10, 100, 101, 102,..., 109, 11, 110, 111, 112, 113, 114, ..., 119, 12, 120
         res = []
-        def build(num):
-            if num > n:
-                return
-            res.append(num)
-            for i in range(10):
-                cur = 10 * num + i
-                if cur > n:
-                    break
-                build(cur)
-            return
-        for j in range(1, 10):
-            build(j)
+        candidates = [0]
+        while candidates:
+            curr = candidates.pop()
+            if curr > 0:
+                res.append(curr)
+            for i in range(9, -1, -1):
+                if curr+i == 0:
+                    continue
+                if curr * 10 + i <= n:
+                    candidates.append(curr * 10 + i)
+        return res
         return res
