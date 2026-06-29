53ebcb30b1654fc7577a7aae24faa7aca52352f0
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-29 15:41:48 +0800

    Time: 0 ms (100%), Space: 19.4 MB (60.75%) - LeetHub

--- LeetCode/Medium/1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.py
+++ LeetCode/Medium/1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.py
@@ -6,25 +6,15 @@
 #         self.right = right
 class Solution:
     def btreeGameWinningMove(self, root: Optional[TreeNode], n: int, x: int) -> bool:
-        self.is_find = False
-        self.x_node = TreeNode()
-        def find(root):
-            if root is None or self.is_find:
-                return
-            if root.val == x:
-                self.x_node = root
-                self.is_find = True
-                return
-            find(root.left)
-            find(root.right)
-            return
-        def count(root):
+        lsz = rsz = 0
+        def dfs(root):
             if root is None:
                 return 0
-            return count(root.left) + count(root.right) + 1
-        find(root)
-        x_left = count(self.x_node.left)
-        x_right = count(self.x_node.right)
-        r = n - (x_left + x_right + 1)
-        return r > n // 2 or x_left > n // 2 or x_right > n // 2
-        # return n > 2 * (x_left + x_right + 1) or max(x_right, x_left) > (min(x_right, x_left) + 1 + r)
+            ls = dfs(root.left)
+            rs = dfs(root.right)
+            if root.val == x:
+                nonlocal lsz, rsz
+                lsz, rsz = ls, rs
+            return ls + rs + 1
+        dfs(root)
+        return max((n - lsz - rsz - 1), lsz, rsz) * 2 > n
