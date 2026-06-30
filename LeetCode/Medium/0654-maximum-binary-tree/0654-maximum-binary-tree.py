c376a7d77cfe3f7a1730b95e9a42ce6037b32fe3
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-06-30 23:56:51 +0800

    Time: 9 ms (95.25%), Space: 19.7 MB (42.74%) - LeetHub

--- LeetCode/Medium/0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ LeetCode/Medium/0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -1,34 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def constructMaximumBinaryTree(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: Optional[TreeNode]
-        """
-        n = len(nums)
-        root = self.construct(nums, 0, n-1)
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        root = None
+        stk = []
+        for num in nums:
+            cur = TreeNode(num)
+            while stk and num > stk[-1].val:
+                cur.left = stk.pop()
+            if stk:
+                stk[-1].right = cur
+            else:
+                root = cur
+            stk.append(cur)
         return root
-    
-    def construct(self, nums, l, r):
-        if l > r:
-            return None
-        idx = self.max_of_nums(nums, l, r)
-        cur = TreeNode(nums[idx])
-        cur.left = self.construct(nums, l, idx-1)
-        cur.right = self.construct(nums, idx+1, r)
-        return cur
-    
-    def max_of_nums(self, nums, l, r):
-        if l == r:
-            return l
-        idx = l
-        for i in range(l+1, r+1):
-            if nums[i] > nums[idx]:
-                idx = i
-        return idx
-    
