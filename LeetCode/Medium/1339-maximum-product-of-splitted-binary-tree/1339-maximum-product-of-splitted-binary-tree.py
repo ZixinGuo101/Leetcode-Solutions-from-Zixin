f81264add72a04685af647225e4c6e909078778f
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-09 11:37:40 +0800

    Time: 53 ms (73.84%), Space: 46.3 MB (75.77%) - LeetHub

--- LeetCode/Medium/1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ LeetCode/Medium/1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -6,21 +6,15 @@
 #         self.right = right
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
-        def dfs1(root):
+        MOD = 10 ** 9 + 7
+        sub_sum = []
+        def getTotal(root):
             if root is None:
                 return 0
-            ls = dfs1(root.left)
-            rs = dfs1(root.right)
-            return root.val + ls + rs
-        totalSum = dfs1(root)
-        self.res = 0
-        def dfs2(root):
-            if root is None:
-                return 0
-            l = dfs2(root.left)
-            r = dfs2(root.right)
-            cur = l + r + root.val
-            self.res = max(self.res, cur * (totalSum - cur))
+            l_sum = getTotal(root.left)
+            r_sum = getTotal(root.right)
+            cur = l_sum + r_sum + root.val
+            sub_sum.append(cur)
             return cur
-        dfs2(root)
-        return self.res % (10 ** 9 + 7)
+        total_sum = getTotal(root)
+        return max(s * (total_sum - s) for s in sub_sum) % MOD
