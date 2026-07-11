431e77b679ceb8b7baf2282bddc9c87969478d0f
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-11 08:48:57 +0800

    Time: 39 ms (97.64%), Space: 47.9 MB (96.46%) - LeetHub

--- LeetCode/Medium/1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ LeetCode/Medium/1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,24 +1,18 @@
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        self.graph = {}
-        self.visited = set()
-        self.visited.add(0)
-        for i in range(n):
-            self.graph[i] = []
-        for edge in edges:
-            self.graph[edge[0]].append(edge[1])
-            self.graph[edge[1]].append(edge[0])
+        self.graph = [[] for _ in range(n)]
+        for a, b in edges:
+            self.graph[a].append(b)
+            self.graph[b].append(a)
         
-        def dfs(r):
-            cur = 0
-            for v in self.graph[r]:
-                if v not in self.visited:
-                    self.visited.add(v)
-                    print(v)
-                    cur += dfs(v)
-                    print(v, cur)
-            if r != 0 and (hasApple[r] or cur > 0):
-                cur += 2
-            return cur
+        def dfs(node, parent):
+            total = 0
+            for child in self.graph[node]:
+                if child == parent:
+                    continue
+                cost = dfs(child, node)
+                if cost > 0 or hasApple[child]:
+                    total += cost + 2
+            return total
         
-        return dfs(0)
+        return dfs(0, -1)
