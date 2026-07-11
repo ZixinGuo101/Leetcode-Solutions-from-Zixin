1b26de8f1b4e528d305945ca0ed6eb57c5a28511
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-11 11:02:57 +0800

    Time: 48 ms (48.24%), Space: 22.2 MB (16.73%) - LeetHub

--- LeetCode/Medium/0687-longest-univalue-path/0687-longest-univalue-path.py
+++ LeetCode/Medium/0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -6,33 +6,21 @@
 #         self.right = right
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        self.max_len = 0
-        '''
-        cur_inc and cur_dec represents 左右子树中能和当前节点接上的相同值
-        recursion 的三部曲：
-        signature : 以当前节点为起点，向子树延伸的相同值路径的最大边数。
-        Sub_Problem : 左右孩子的最大值
-        Recursion Step : 左右孩子的最大值 + 1
-        '''
-        def dfs(node):
-            if not node:
+        self.res = 0
+        def dfs(root):
+            if root is None:
                 return 0
-
-            left_length = dfs(node.left)
-            right_length = dfs(node.right)
-
-            arrow_left = 0
-            arrow_right = 0
-
-            if node.left and node.val == node.left.val:
-                arrow_left = left_length + 1
-
-            if node.right and node.val == node.right.val:
-                arrow_right = right_length + 1
-            
-            self.max_len = max(arrow_left + arrow_right, self.max_len)
-            return max(arrow_left,arrow_right)
-        
+            lv = dfs(root.left)
+            rv = dfs(root.right)
+            if root.left and root.left.val == root.val:
+                lv += 1
+            else:
+                lv = 0
+            if root.right and root.right.val == root.val:
+                rv += 1
+            else:
+                rv = 0
+            self.res = max(self.res, lv + rv)
+            return max(lv, rv)
         dfs(root)
-        return self.max_len
-        
+        return self.res
