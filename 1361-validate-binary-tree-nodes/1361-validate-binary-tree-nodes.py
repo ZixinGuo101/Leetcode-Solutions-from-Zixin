8542f836b6867f45488b7313dc8c37b4de89ea4d
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-28 20:59:17 +0800

    Time: 14 ms (89.64%), Space: 21.8 MB (59.84%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,26 +1,26 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
-        self.count = n
-        self.parent = [i for i in range(n)]
+        children = set(leftChild + rightChild)
+        if len(children) != n:
+            return False
+        root = -1
         for i in range(n):
-            if not (self.merge(i, leftChild[i]) and self.merge(i, rightChild[i])):
-                return False
-        return self.count == 1
-    
-    def merge(self, i, j):
-        if j == -1:
-            return True
-        if self.parent[j] != j:
-            return False
-        root_i = self.find(i)
-        if root_i == j:
-            return False
-        self.parent[j] = root_i
-        self.count -= 1
-        print(self.parent[j])
-        return True
-    
-    def find(self, i):
-        if self.parent[i] != i:
-            self.parent[i] = self.find(self.parent[i])
-        return self.parent[i]
+            if i not in children:
+                root = i
+                break
+        visited = set()
+        count = 0
+        q = deque([root])
+        while q:
+            cur = q.popleft()
+            if leftChild[cur] != -1:
+                if leftChild[cur] in visited:
+                    return False
+                visited.add(leftChild[cur])
+                q.append(leftChild[cur])
+            if rightChild[cur] != -1:
+                if rightChild[cur] in visited:
+                    return False
+                visited.add(rightChild[cur])
+                q.append(rightChild[cur])
+        return len(visited) == n-1
