0a4754f7c850c27d899a05dcb703cb6a99583f99
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-29 12:25:30 +0800

    Time: 62 ms (75.66%), Space: 21.7 MB (48.67%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,31 +1,18 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        graph = defaultdict(list)
+        in_degree = {}
+        for r, ing in zip(recipes, ingredients):
+            for s in ing:
+                graph[s].append(r)
+            in_degree[r] = len(ing)
+        q = deque(supplies)
         res = []
-        d_re = dict()
-        d_su = dict()
-        q = deque()
-        num = 0
-        for recipe in recipes:
-            d_re[recipe] = num
-            num += 1
-        for supply in supplies:
-            d_su[supply] = num
-            q.append(num)
-            num += 1
-        graph = [[] for _ in range(len(recipes)+len(supplies))]
-        in_degree = [0] * len(recipes)
-        for i, ig in enumerate(ingredients):
-            for food in ig:
-                if food in d_re:
-                    graph[d_re[food]].append(i)
-                elif food in d_su:
-                    graph[d_su[food]].append(i)
-                in_degree[i] += 1
         while q:
             cur = q.popleft()
-            for nxt in graph[cur]:
-                in_degree[nxt] -= 1
-                if in_degree[nxt] == 0:
-                    q.append(nxt)
-                    res.append(recipes[nxt])
+            for node in graph[cur]:
+                in_degree[node] -= 1
+                if in_degree[node] == 0:
+                    q.append(node)
+                    res.append(node)
         return res
