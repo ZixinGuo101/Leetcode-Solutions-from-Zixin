d97481a6c23539dd731be204bd005f92cdd77c95
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-29 11:32:30 +0800

    Time: 6 ms (31.16%), Space: 20.5 MB (43.43%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        in_degree = [0] * numCourses
+        for pre in prerequisites:
+            graph[pre[1]].append(pre[0])
+            in_degree[pre[0]] += 1
+        topo_order = []
+        q = deque(i for i, d in enumerate(in_degree) if d == 0)
+        while q:
+            cur = q.popleft()
+            topo_order.append(cur)
+            for nxt in graph[cur]:
+                in_degree[nxt] -= 1
+                if in_degree[nxt] == 0:
+                    q.append(nxt)
+        return [] if len(topo_order) < numCourses else topo_order
