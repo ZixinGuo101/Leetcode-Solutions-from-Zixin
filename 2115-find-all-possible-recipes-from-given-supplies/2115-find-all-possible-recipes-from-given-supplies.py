99d919421fbad2f51cc8a9bd06c002b871b3108b
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-29 12:31:48 +0800

    Time: 22 ms (98.54%), Space: 21.5 MB (88.38%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,18 +1,47 @@
+#top1
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        graph = defaultdict(list)
-        in_degree = {}
-        for r, ing in zip(recipes, ingredients):
-            for s in ing:
-                graph[s].append(r)
-            in_degree[r] = len(ing)
-        q = deque(supplies)
-        res = []
-        while q:
-            cur = q.popleft()
-            for node in graph[cur]:
-                in_degree[node] -= 1
-                if in_degree[node] == 0:
-                    q.append(node)
-                    res.append(node)
-        return res
+        self.cookbook = {}
+        for recipe, ingredient in zip(recipes, ingredients):
+            self.cookbook[recipe] = ingredient
+		# recipes are mapped to ingredients in dictionary for easy lookup
+		
+        self.set_supplies = set(supplies)
+        # transform to set for easy lookup
+        self.visited_recipes = {}
+        self.canCook = set()
+        for recipe, recipe_ingredients in self.cookbook.items():
+            self.checkRecipe(recipe, recipe_ingredients)
+
+        return list(self.canCook)
+		
+    def checkRecipe(self, recipe: str, ingredients: List[str]) -> Bool:
+        # see if we have already checked this recipe first
+        if recipe in self.visited_recipes and self.visited_recipes[recipe] == 0:
+            return False
+
+        if recipe in self.visited_recipes and self.visited_recipes[recipe] == 1:
+            return True
+        
+        if recipe in self.visited_recipes and self.visited_recipes[recipe] == -1:
+            return False
+
+        self.visited_recipes[recipe] = 0
+        for ingredient in ingredients:
+            if ingredient not in self.set_supplies:
+                # ingredient is not in the supplies list, but maybe its a recipe itself
+                if ingredient not in self.cookbook:
+                    # this is not a recipe, its an ingredient we dont have
+                    self.visited_recipes[recipe] = -1
+                    return False
+                else:
+                # now need to check if this recipe can be made
+                    if ingredient not in self.canCook:
+                        # which check to see if we already know we can make this one
+                        if not self.checkRecipe(ingredient, self.cookbook[ingredient]):
+                            self.visited_recipes[recipe] = -1
+                            return False
+        # all ingredients are checked, none have failed
+        self.canCook.add(recipe)
+        self.visited_recipes[recipe] = 1
+        return True
