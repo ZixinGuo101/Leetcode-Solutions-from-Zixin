30e9e5fdb9ca1f013177054bad86b18f71e293be
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-07-31 12:53:15 +0800

    Time: 413 ms (65.81%), Space: 38 MB (92.59%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -0,0 +1,37 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.n = len(nums) + 1
+        self.tree = [0] * self.n
+        self.nums = [0] * (self.n - 1)
+        for i, num in enumerate(nums):
+            self.update(i, num)
+    
+    def lowbit(self, index: int) -> int:
+        return index & (-index)
+
+    def update(self, index: int, val: int) -> None:
+        delta = val - self.nums[index]
+        self.nums[index] = val
+        index += 1
+        while index < self.n:
+            self.tree[index] += delta
+            index += self.lowbit(index)
+        
+    def query(self, index: int) -> int:
+        total = 0
+        while index > 0:
+            total += self.tree[index]
+            index -= self.lowbit(index)
+        return total
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.query(right + 1) - self.query(left)        
+
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(index,val)
+# param_2 = obj.sumRange(left,right)
