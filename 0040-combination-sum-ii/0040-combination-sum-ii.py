3bed5711cba2e72e2860559ac593f488c66f6692
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-01 13:05:30 +0800

    Time: 31 ms (19.63%), Space: 19.5 MB (41.98%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        combination = []
+        self.total = 0
+        n = len(candidates)
+        def backtrack(start):
+            if self.total == target:
+                res.append(combination.copy())
+                return
+            if self.total > target:
+                return
+            for i in range(start, n):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                combination.append(candidates[i])
+                self.total += candidates[i]
+                backtrack(i + 1)
+                self.total -= candidates[i]
+                combination.pop()
+        backtrack(0)
+        return res
