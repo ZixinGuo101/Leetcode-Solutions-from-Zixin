c5cd812ede087807688a18bebedb68079e070926
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-01 10:27:34 +0800

    Time: 2 ms (36.92%), Space: 19.4 MB (86.87%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,22 +2,18 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         n = len(nums)
-        on_path = [False] * n
+        visited = [False] * n
         path = []
-        def traverse(i, t):
-            if on_path[i]:
-                return
-            if t == n:
-                path.append(nums[i])
+        def backtrack():
+            if len(path) == n:
                 res.append(path.copy())
+            for i in range(n):
+                if visited[i]:
+                    continue
+                path.append(nums[i])
+                visited[i] = True
+                backtrack()
+                visited[i] = False
                 path.pop()
-                return
-            on_path[i] = True
-            path.append(nums[i])
-            for j in range(n):
-                traverse(j, t+1)
-            on_path[i] = False
-            path.pop()
-        for i in range(n):
-            traverse(i, 1)
+        backtrack()
         return res
