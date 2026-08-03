2940e8232cac9fd1785c30ea1ffc431e443aaa4f
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-03 23:32:04 +0800

    Time: 284 ms (95.86%), Space: 39.1 MB (47.54%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,40 +1,37 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.n = len(nums) + 1
-        self.tree = [0] * self.n
-        for i in range(1, self.n):
-            self.tree[i] += nums[i - 1]
-            j = i + self.lowbit(i)
-            if j < self.n:
-                self.tree[j] += self.tree[i]
-        print(self.tree)
-        self.nums = nums[:]
-        # for i, num in enumerate(nums):
-            # self.update(i, num)
-    
-    def lowbit(self, index: int) -> int:
-        return index & (-index)
+        self._nums = nums
+        self._tree = [ 0, *nums ]
 
-    def update(self, index: int, val: int) -> None:
-        delta = val - self.nums[index]
-        self.nums[index] = val
+        for index, number in enumerate(self._tree):
+            index += index & -index
+            if index < len(self._tree):
+                self._tree[index] += number
+
+    def update(self, index: int, val: int):
+        previous = self._nums[index]
+        self._nums[index] = val
+
+        diff = val - previous
         index += 1
-        while index < self.n:
-            self.tree[index] += delta
-            index += self.lowbit(index)
-        
+        while index < len(self._tree):
+            self._tree[index] += diff
+            index += index & -index
+
+
     def query(self, index: int) -> int:
-        total = 0
+        index += 1
+        value = 0
+
         while index > 0:
-            total += self.tree[index]
-            index -= self.lowbit(index)
-        return total
+            value += self._tree[index]
+            index -= index & -index
+
+        return value
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.query(right + 1) - self.query(left)        
-
-        
+        return self.query(right) - self.query(left-1)
 
 
 # Your NumArray object will be instantiated and called as such:
