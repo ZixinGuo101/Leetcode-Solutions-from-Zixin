a0bd6b6729d1da3edee740468271386604eb042b
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-05 23:57:43 +0800

    Time: 335 ms (70.06%), Space: 21.1 MB (77.63%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -5,15 +5,15 @@ class Solution:
             g[u].append((v, w))
         t = [float('inf')] * (n + 1)
         t[0] = t[k] = 0
-        q = [(k, 0)]
+        q = [(0, k)]
         while q:
-            cur_node, cur_time = heapq.heappop(q)
+            cur_time, cur_node = heapq.heappop(q)
             if t[cur_node] < cur_time:
                 continue
             for next_node, next_time in g[cur_node]:
                 next_time += cur_time
                 if next_time < t[next_node]:
                     t[next_node] = next_time
-                    heapq.heappush(q, (next_node, next_time))
+                    heapq.heappush(q, (next_time, next_node))
         mx = max(t)
         return -1 if mx == float('inf') else mx
