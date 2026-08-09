f6b1412d4d3e8c367379bddba8661158d0df0639
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-09 18:22:54 +0800

    Time: 5 ms (86.17%), Space: 19.4 MB (75.27%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,38 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.trie = {}
-        self.ans = []
-    
-    def build_trie(self, words: List[str]):
-        for word in words:
-            p = self.trie
-            for char in word:
-                p[char] = p.setdefault(char, {})
-                p = p[char]
-            p[''] = True
-        return
-    
-    def find(self, d: dict, path: List[str]):
-        if '' not in d:
-            return
-        
-        self.compare_path(path)
-        for key in sorted(d):
-            path.append(key)
-            if key != '':
-                self.find(d[key], path)
-            path.pop()
-
-    
-    def compare_path(self, path: List[str]):
-        if len(path) > len(self.ans):
-            self.ans = path[:]
-            return
-
     def longestWord(self, words: List[str]) -> str:
-        self.build_trie(words)
-        for key in sorted(self.trie):
-            path = [key]
-            self.find(self.trie[key], path)
-        return ''.join(self.ans)
+        words.sort()
+        s = set()
+        s.add("")
+        ans = ""
 
+        for word in words:
+            if word[:-1] in s:
+                s.add(word)
+                if len(word) > len(ans):
+                    ans = word
+        
+        return ans
