7ca0938193e4fc030573f1a0aef334aa294c8bab
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-10 20:50:31 +0800

    Time: 2458 ms (22.02%), Space: 81.1 MB (74.85%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,36 +1,19 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.n = n
-    def find(self, x):
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX == rootY:
-            return False
-        self.parent[self.find(x)] = self.find(y)
-        self.n -= 1
-        return True
-    def count(self):
-        return self.n
-
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        edges = []
+        def distance(x, y):
+            d = abs(points[x][0] - points[y][0]) + abs(points[x][1] - points[y][1])
+            return d
         n = len(points)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
-                edges.append((d, i, j))
-        edges.sort(key = lambda e: e[0])
-        uf = UnionFind(n)
         sum_wt = 0
-        for d, x, y in edges:
-            if uf.union(x, y):
-                sum_wt += d
-            if uf.count() == 1:
-                break
+        visited = [False] * n
+        q = [(0, 0)]
+        while q:
+            cur_w, cur_v = heapq.heappop(q)
+            if visited[cur_v]:
+                continue
+            visited[cur_v] = True
+            sum_wt += cur_w
+            for nxt_v in range(n):
+                if not visited[nxt_v]:
+                    heapq.heappush(q, (distance(cur_v, nxt_v), nxt_v))
         return sum_wt
