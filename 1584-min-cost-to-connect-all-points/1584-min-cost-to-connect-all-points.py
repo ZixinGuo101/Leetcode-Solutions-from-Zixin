d4a2526b2a82bd8456d53f97d69883a843908769
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-10 22:11:58 +0800

    Time: 288 ms (98.74%), Space: 19.4 MB (97.83%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -4,16 +4,25 @@ class Solution:
             d = abs(points[x][0] - points[y][0]) + abs(points[x][1] - points[y][1])
             return d
         n = len(points)
-        sum_wt = 0
-        visited = [False] * n
-        q = [(0, 0)]
-        while q:
-            cur_w, cur_v = heapq.heappop(q)
-            if visited[cur_v]:
-                continue
-            visited[cur_v] = True
-            sum_wt += cur_w
-            for nxt_v in range(n):
-                if not visited[nxt_v]:
-                    heapq.heappush(q, (distance(cur_v, nxt_v), nxt_v))
-        return sum_wt
+        total = 0
+        min_dist = [float('inf')] * n
+        remaining = set([i for i in range(n)])
+        cur = 0
+        remaining.remove(cur)
+        min_dist[cur] = 0
+        while remaining:
+            best_v = -1
+            best_d = float('inf')
+            for v in remaining:
+                d = distance(cur, v)
+                if d < min_dist[v]:
+                    min_dist[v] = d
+                if min_dist[v] < best_d:
+                    best_d = min_dist[v]
+                    best_v = v
+            if best_v == -1:
+                return -1
+            total += best_d
+            cur = best_v
+            remaining.remove(cur)
+        return total
