c0dc2facfb3cb147aa76d35b097037b576c7f3c6
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-14 22:32:14 +0800

    Time: 1994 ms (5.01%), Space: 732.9 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
-        for money in range(1, len(dp)):
+        if amount == 0:
+            return 0
+        mask = (1 << (1 + amount)) - 1
+        target = 1 << amount
+        seen = 1
+        reach = 1
+        step = 0
+        while reach:
+            nxt = 0
+            step += 1
             for coin in coins:
-                if money < coin:
-                    continue
-                dp[money] = min(dp[money], dp[money - coin] + 1)
-        return dp[-1] if dp[-1] < amount + 1 else -1
+                nxt |= reach << coin
+            nxt &= mask
+            nxt &= ~seen
+            if nxt & target:
+                return step
+            if nxt == 0:
+                return -1
+            seen |= nxt
+            reach = nxt
+        return -1
+
