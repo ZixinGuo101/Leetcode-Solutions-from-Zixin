f2a808133f9350fb609d1169954fcb57c30c01c1
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-14 22:42:49 +0800

    Time: 6 ms (99.95%), Space: 19.7 MB (52.46%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,6 +2,18 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
+        coins = list(set(coin for coin in coins if coin <= amount))
+        if not coins:
+            return -1
+        if len(coins) == 1:
+            return amount // coins[0] if amount % coins[0] == 0 else -1
+        g = reduce(gcd, coins)
+        if amount % g:
+            return -1
+        if g > 1:
+            amount //= g
+            coins = [coin // g for coin in coins ]
+
         mask = (1 << (1 + amount)) - 1
         target = 1 << amount
         seen = 1
