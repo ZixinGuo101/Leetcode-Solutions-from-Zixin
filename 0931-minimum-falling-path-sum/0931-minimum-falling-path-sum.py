da5e67e73cc8810b86a34d131da1cb941b042c75
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-08-16 22:26:26 +0800

    Time: 9 ms (92.84%), Space: 20.2 MB (60.72%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -4,13 +4,10 @@ class Solution:
         dp = matrix[0][:]
         for i in range(1, n):
             nxt_dp = [0] * n
-            for j in range(n):
-                mn = dp[j]
-                if j > 0:
-                    mn = min(mn, dp[j - 1])
-                if j < n - 1:
-                    mn = min(mn, dp[j + 1])
-                nxt_dp[j] = mn + matrix[i][j]
+            nxt_dp[0] = matrix[i][0] + min(dp[0], dp[1])
+            for j in range(1, n - 1):
+                nxt_dp[j] =matrix[i][j] + min(dp[j-1], dp[j], dp[j+1])
+            nxt_dp[-1] = matrix[i][-1] + min(dp[-1], dp[-2])
             dp = nxt_dp
         print(dp)
         return min(dp)
