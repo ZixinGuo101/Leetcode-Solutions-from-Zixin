bf5f1baad8312876546513609203609daca4cee2
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-09-04 09:39:32 +0800

    Time: 691 ms (54.59%), Space: 19.3 MB (69.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -5,16 +5,12 @@ class Solution:
         if total & 1:
             return False
         total >>= 1
-        dp = [[False] * (total + 1) for _ in range(n + 1)]
-        dp[0][0] = True
+        dp = [False] * (total + 1)
+        dp[0] = True
         temp_sum = 0
-        for i in range(1, n + 1):
-            x = nums[i - 1]
-            temp_sum += x
-            for j in range(1, total + 1):
-                if j > temp_sum:
-                    break
-                dp[i][j] = dp[i - 1][j]
-                if j >= x:
-                    dp[i][j] |= dp[i - 1][j - x]
-        return dp[n][total]
+        for num in nums:
+            temp_sum += num
+            end = min(temp_sum, total)
+            for i in range(end, num - 1, -1):
+                dp[i] |= dp[i - num]
+        return dp[total]
