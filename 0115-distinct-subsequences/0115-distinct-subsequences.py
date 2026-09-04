70290ba30346fd1a6fc7a8e40ffbd9695480fea8
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-09-04 09:00:00 +0800

    Time: 15 ms (98.17%), Space: 43.7 MB (65.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -5,17 +5,17 @@ class Solution:
         # if m < n:
         #     return False
         
-        memo = {}
+        memo = [[-1] * n for _ in range(m)]
         
         def dfs(i, j):
             if i < 0 or j < 0 or i < j:
                 return int(j == -1)
-            if (i, j) in memo:
-                return memo[(i, j)]
+            if memo[i][j] != -1:
+                return memo[i][j]
             ans = dfs(i - 1, j)
             if s[i] == t[j]:
                 ans += dfs(i - 1, j - 1)
-            memo[(i, j)] = ans
+            memo[i][j] = ans
             return ans
 
         return dfs(m-1, n-1)
