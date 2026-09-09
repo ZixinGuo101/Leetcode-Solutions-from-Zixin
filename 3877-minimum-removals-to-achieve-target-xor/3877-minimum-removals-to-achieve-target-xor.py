5a21e5455471c7eeb2ce8372bdeb17513990f99d
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-09-09 08:28:17 +0800

    Time: 783 ms (50.94%), Space: 299.5 MB (15.1%) - LeetHub

---
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minRemovals(self, nums: List[int], target: int) -> int:
+        memo = {}
+        n = len(nums)
+
+        def dfs(i, c):
+            if i < 0:
+                return 0 if c == 0 else inf
+            if (i, c) in memo:
+                return memo[(i,c)]
+            res = min(dfs(i-1, c) + 1, dfs(i-1, c^nums[i]))
+            memo[(i, c)] = res
+            return res
+        
+        ans = dfs(n-1, target)
+        return ans if ans != inf else -1
