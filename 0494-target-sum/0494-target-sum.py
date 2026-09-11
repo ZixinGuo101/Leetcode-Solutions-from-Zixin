cb3270f81e768d4d6a19960f5e9c9a87d1486e23
ZixinGuo101 <62042420+ZixinGuo101@users.noreply.github.com>
2026-09-11 11:13:45 +0800

    Time: 15 ms (97.75%), Space: 19.2 MB (98.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,14 +1,21 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
         total = sum(nums)
-        if total < abs(target) or (total + target) & 1:
+        if total < abs(target) or ((total +target) & 1):
             return 0
-
-        m = (total + target) >> 1
-        dp = [0] * (m + 1)
+        n = len(nums)
+        t = (total + target) // 2
+        post_sum = [0] * n
+        post_sum[n-1] = nums[n-1]
+        for i in range(n-2, -1, -1):
+            post_sum[i] = nums[i] + post_sum[i+1]
+        # print(post_sum)
+        dp = [0] * (t + 1)
         dp[0] = 1
-        for num in nums:
-            for i in range(m, num-1, -1):
-                dp[i] += dp[i-num]
-        return dp[m]
+        for i in range(1, n):
+            r_bound = max(t - post_sum[i], nums[i-1])
+            for j in range(t, r_bound - 1, -1):
+                dp[j] += dp[j - nums[i-1]]
+            # print(dp)
+            # print(r_bound)
+        return dp[t] if (t - nums[n-1] < 0) else dp[t] + dp[t - nums[n-1]]
